435d8a5b438d535ebae4f0fc19845dd56e93fa9c
KevinChiangMyFeel <36830442+kevin77836@users.noreply.github.com>
2025-08-11 13:07:30 +0800

    update

---
+++ pages/news/newsPage.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="p-news-page">
+        <section class="page-section">
+            <div class="section-wrapper">
+                <div class="news-date">
+                    2025/4/7
+                </div>
+                <div class="c-title title-grey">
+                    考試達人分享會四月份場次開始報名囉
+                </div>
+                <div class="page-content">
+                    <div>
+                        為了英檢考試所苦嗎? 差一點點就達到門檻嗎?<br>
+                        讓考試達人們陪你一起面對各種考試<br>
+                        本次活動精選托福、雅思、GRE學生達人，真實的考試心得，分享獨門高分秘笈!<br>
+                        馬上報名吧 https://forms.gle/WvToYQRkCjVP6dH88 
+                    </div>
+                    <div class="page-img">
+                        <img src="/news-page/news-img.jpg" alt="">
+                    </div>
+                </div>
+                <div class="cta">
+                    <a href="#" class="c-button">
+                        <div class="hero-cta-text">
+                            更多消息
+                        </div>
+                        <div class="hero-cta-img">
+                            <img src="/icon-arrow.svg" alt="">
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
